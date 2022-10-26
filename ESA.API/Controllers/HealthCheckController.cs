f65cb9daf3f2aed35a95ac65e5f7dd386d506841
Deybis Chavez <deybischavez7@gmail.com>
2022-10-26 14:53:45 -0600

    added healthcheck method

--- ESA.API/Controllers/HealthCheckController.cs
+++ ESA.API/Controllers/HealthCheckController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ESA.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class HealthCheckController : ControllerBase
     {
