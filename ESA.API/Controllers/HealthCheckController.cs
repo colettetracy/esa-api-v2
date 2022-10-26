7a322a523b7f5feb247fbc134512347092f2bc05
Deybis Chavez <deybischavez7@gmail.com>
2022-10-26 14:39:20 -0600

    added healthcheck method

---
+++ ESA.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using ESA.Core.Models.Student;
+using ESA.Core.Services;
+using ESA.Core.Specs.Filters;
+using GV.DomainModel.SharedKernel.Interop;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
